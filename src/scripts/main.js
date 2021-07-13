ca3b162a94b6b13a1009c19f80a1272b7346c6b7
VadymMakevytss <vadymlat@mail.ru>
2021-07-13 14:06:25 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const toggler = document.querySelector('.header__toggler');
-const nav = document.querySelector('.nav');
-
-toggler.addEventListener('click', (events) => {
-  nav.classListadd('red');
-});
